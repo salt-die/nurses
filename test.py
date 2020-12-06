f176fc1881c85a01f0760ff53b4e7dc4a4961e8f
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-06 14:39:15 -0600

    new test

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
+from random import random
 import time
+
 from nurses import ScreenManager
-import numpy as np
+
 
 with ScreenManager() as sm:
-    my_widget = sm.new_widget(5, 5, 4, 15)
-    my_widget[1:3, 1:14] = "Hello, World!\nI'm a widget!"
-    my_widget.colors[1:3, 5:-5] = sm.color(3)
-    my_widget.border("curved", sm.color(2))
+    widgets = [sm.new_widget(5 * i, 0, 4, 15) for i in range(5)]
+    for widget in widgets:
+        widget[1:3, 1:14] = "Hello, World!\nI'm a widget!"
+        widget.colors[1:3, 5:-5] = sm.color(3)
+        widget.border("curved", sm.color(2))
 
     for i in range(30):
-        my_widget.ul = i, i
-        my_widget.buffer = np.roll(my_widget.buffer, (1, 0), (1, 0))  # Applying numpy functions to the buffer for fun effects
-        my_widget.refresh()
+        for widget in widgets:
+            widget.left += round(random())
+            widget.top += round(random())
+        sm.refresh()
         time.sleep(.2)
