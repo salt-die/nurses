cc1295409540e8001fb4c9795bc4c2bdb7bfbf2a
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-28 09:11:29 -0600

    automatic offset in __setitem__ and __getitem__ if has_border is truth-y

--- nurses/widget.py
+++ nurses/widget.py
@@ -38,8 +38,8 @@ class Widget:
 
         self.default_color = curses.color_pair(0) if default_color is None else default_color
 
+        self.has_border = False
         self.buffer = np.full((height, width), " ")
-
         if colors := kwargs.get("colors"):
             self.colors = colors
         else:
@@ -65,38 +65,66 @@ class Widget:
         self._width = val
         self._resize()
 
+    @property
+    def colors(self):
+        return self._colors[1: -1, 1: -1] if self.has_border else self._colors
+
+    @colors.setter
+    def colors(self, array):
+        if self.has_border:
+            self._colors[1: -1, 1: -1] = array
+        else:
+            self._colors = array
+
+    @property
+    def buffer(self):
+        return self._buffer[1: -1, 1: -1] if self.has_border else self._buffer
+
+    @buffer.setter
+    def buffer(self, array):
+        if self.has_border:
+            self._buffer[1: -1, 1: -1] = array
+        else:
+            self._buffer = array
+
     def _resize(self):
         height, width = self.height, self.width
-        old_h, old_w = self.buffer.shape
+        old_h, old_w = self._buffer.shape
         min_h, min_w = min(height, old_h), min(width, old_w)
 
         new_buffer = np.full((height, width), " ")
-        new_buffer[:min_h, :min_w] = self.buffer[:min_h, :min_w]
+        new_buffer[:min_h, :min_w] = self._buffer[:min_h, :min_w]
 
         new_colors = np.full((height, width), self.default_color)
-        new_colors[:min_h, :min_w] = self.colors[:min_h, :min_w]
+        new_colors[:min_h, :min_w] = self._colors[:min_h, :min_w]
 
-        self.buffer = new_buffer
-        self.colors = new_colors
+        self._buffer = new_buffer
+        self._colors = new_colors
         self.window.erase()
         self.window.resize(height, width + 1)
-        self.refresh()
+        if self.has_border:
+            self.border(*self.has_border)
+        else:
+            self.refresh
 
     def refresh(self):
         """Write the buffers to the window."""
-        it = np.nditer((self.buffer, self.colors), ["multi_index"])
+        it = np.nditer((self._buffer, self._colors), ["multi_index"])
         for char, color in it:
             y, x = it.multi_index
             self.window.addstr(y, x, str(char), color)
 
     def __getitem__(self, key):
+        """buffer.__getitem__ except offset if self.has_border is truth-y."""
         return self.buffer[key]
 
     def __setitem__(self, key, item):
         """buffer.__setitem__ except in cases where item is a string.
         If item is a string of length > 1: coerce string into a tuple or tuple of tuples.
         This convenience will allow one to update text on a widget more directly:
-            my_widget[2:4, :13] = "Hello, World!\nI'm a widget!"
+            `my_widget[2:4, :13] = "Hello, World!\nI'm a widget!"`
+
+        If self.has_border is truth-y then indices will be offset automatically.
         """
         if isinstance(item, str):
             if "\n" in item:
@@ -107,49 +135,52 @@ class Widget:
         try:
             self.buffer[key] = item
         except ValueError:
-            self.buffer[key] = item.T  if len(item.shape) == 2 else item[None, ].T  # Try to fit the text vertically
+            self.buffer[key] = item.T if len(item.shape) == 2 else item[None, ].T  # Try to fit the text vertically
 
         self.refresh()
 
-    def border(self, style="light", color=None):
+    def border(self, style="light", color=None, *, read_only=True):
         """Draw a border on the edges of the widget.
            `style` can be one of ["light", "heavy", "double", "curved"]
-        """
-        ul, ur, v, h, ll, lr = BORDER_STYLES[style]
 
-        self[0] = h
-        self[-1] = h
-        self[:, 0] = v
-        self[:, -1] = v
-        self[0, 0] = ul
-        self[0, -1] = ur
-        self[-1, 0] = ll
-        self[-1, -1] = lr
+           Calling this method sets the attribute `has_border` to (style, color).
+           Methods such as __getitem__, roll, scroll, resize will take care to preserve the border
+           as long as `has_border` is truth-y.  To disable this behavior set `has_border` to False
+           or call this method with `read_only=False`.
+        """
+        self.has_border = style, color
+
+        ul, ur, v, h, ll, lr = BORDER_STYLES[style]
+        b = self._buffer
+
+        b[(0, -1), :] = h
+        b[:, (0, -1)] = v
+        b[ 0,  0] = ul
+        b[ 0, -1] = ur
+        b[-1,  0] = ll
+        b[-1, -1] = lr
 
         if color is not None:
-            c = self.colors
+            c = self._colors
             c[0] = c[-1] = c[:, 0] = c[:, -1] = color
 
         self.refresh()
 
-    def roll(self, shift=1, vertical=False, roll_border=True):
+    def roll(self, shift=1, vertical=False):
+        """Roll the contents of the buffer.
+           Items that roll beyond the last position are re-introduced at the first.
+        """
         axis = (-shift, 0) if vertical else (0, -shift)
-        if roll_border:
-            self.buffer = np.roll(self.buffer, axis, (0, 1))
-            self.colors = np.roll(self.colors, axis, (0, 1))
-        else:
-            self.buffer[1: -1, 1: -1] = np.roll(self.buffer[1: -1, 1: -1], axis, (0, 1))
-            self.colors[1: -1, 1: -1] = np.roll(self.colors[1: -1, 1: -1], axis, (0, 1))
+        self.buffer = np.roll(self.buffer, axis, (0, 1))
+        self.colors = np.roll(self.colors, axis, (0, 1))
         self.refresh()
 
-    def scroll(self, scroll_border=True):
-        self.roll(vertical=True, roll_border=scroll_border)
-        if scroll_border:
-            self.buffer[-1] = " "
-            self.colors[-1] = self.default_color
-        else:
-            self.buffer[-2, 1: -1] = " "
-            self.colors[-2, 1: -1] = self.default_color
+    def scroll(self):
+        """Scroll the contents of the buffer upwards, erasing the last line.
+        """
+        self.roll(vertical=True)
+        self.buffer[-1] = " "
+        self.colors[-1] = self.default_color
         self.refresh()
 
     def on_press(self, key):
--- test.py
+++ test.py
@@ -15,22 +15,21 @@ async def random_walk():
 async def roll():
     for _ in range(150):
         for widget in sm.group("moving"):
-            widget.roll(roll_border=False)
+            widget.roll()
         sm.refresh()
         await sm.sleep(.1)
 
 async def resize():
     for _ in range(15):
         resize_widget.width -= 1
-        resize_widget.border("curved", sm.colors.RED_ON_BLACK)
         sm.refresh()
         await sm.sleep(1)
 
 async def scroll():
     for i in range(15):
-        scroll_widget.scroll(scroll_border=False)
-        scroll_widget.colors[-2, 1: -1] = sm.colors.BLUE_ON_BLACK
-        scroll_widget[-2, 1: -1] = f"New Text{i:02}"
+        scroll_widget.scroll()
+        scroll_widget.colors[-1] = sm.colors.BLUE_ON_BLACK
+        scroll_widget[-1] = f"New Text{i:02}"
         sm.refresh()
         await sm.sleep(1)
 
@@ -40,17 +39,17 @@ sm.colors.PURPLE = 103, 15, 215
 for i in range(5):
     widget = sm.new_widget(i * 5, 0, 4, 15, group="moving")
     widget.border(color=sm.colors.PURPLE_ON_BLACK)
-    widget[1:-1, 1:-1] = "Hello, World!\nI'm a widget!"
-    widget.colors[1, 1: -1] = sm.colors.RED_ON_BLACK
+    widget[:] = "Hello, World!\nI'm a widget!"
+    widget.colors[0] = sm.colors.RED_ON_BLACK
 
 resize_widget = sm.new_widget(9, 40, 12, 18, default_color=sm.colors.YELLOW_ON_BLACK)
 resize_widget.border("curved", sm.colors.RED_ON_BLACK)
-resize_widget[1: -1, 1: -1] = "Resize me!"
+resize_widget[:] = "Resize me!"
 resize_widget.colors[:, ::2] = sm.colors.BLACK_ON_YELLOW
 
 scroll_widget = sm.new_widget(9, 60, 18, 12)
 scroll_widget.border("double", sm.colors.CYAN_ON_BLACK)
-scroll_widget[1: -1, 1: -1] = "Scroll me!"
+scroll_widget[:] = "Scroll me!"
 
 with sm:  # alternatively, manually close with sm.close()
     sm.run_soon(random_walk(), roll(), resize(), scroll())
