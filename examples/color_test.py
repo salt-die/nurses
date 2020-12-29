17171ff60cd93fe622c739f10c0871f5d97dba81
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-29 07:48:44 -0600

    trimming

--- examples/color_test.py
+++ examples/color_test.py
@@ -1,32 +1,29 @@
 from nurses import ScreenManager
 
-sm = ScreenManager()
+with ScreenManager() as sm:
+    # Define some new colors:
+    sm.colors.PURPLE = 103, 15, 215
+    sm.colors.FUCHSIA = 181, 52, 78
+    sm.colors.ORANGE = 224, 132, 33
+    sm.colors.TEAL = 17, 163, 112
 
-# Define some new colors:
-sm.colors.PURPLE = 103, 15, 215
-sm.colors.FUCHSIA = 181, 52, 78
-sm.colors.ORANGE = 224, 132, 33
-sm.colors.TEAL = 17, 163, 112
+    HEIGHT, WIDTH = 10, 11
+    widget = sm.new_widget(10, 10, HEIGHT, WIDTH)
+    widget.colors[0::4] = sm.colors.PURPLE_ON_BLACK
+    widget.colors[1::4] = sm.colors.FUCHSIA_ON_YELLOW
+    widget.colors[2::4] = sm.colors.ORANGE_ON_GREEN
+    widget.colors[3::4] = sm.colors.TEAL_ON_WHITE
+    widget[:] = "Color Test!"
 
-HEIGHT, WIDTH = 10, 11
-widget = sm.new_widget(10, 10, HEIGHT, WIDTH)
-widget.colors[0::4] = sm.colors.PURPLE_ON_BLACK
-widget.colors[1::4] = sm.colors.FUCHSIA_ON_YELLOW
-widget.colors[2::4] = sm.colors.ORANGE_ON_GREEN
-widget.colors[3::4] = sm.colors.TEAL_ON_WHITE
-widget[:] = "Color Test!"
+    async def roll_and_scroll():
+        for _ in range(WIDTH):
+            sm.refresh()
+            widget.roll()
+            await sm.sleep(.2)
 
-async def roll_and_scroll():
-    for _ in range(WIDTH):
-        sm.refresh()
-        widget.roll()
-        await sm.sleep(.2)
+        for _ in range(HEIGHT):
+            sm.refresh()
+            widget.scroll()
+            await sm.sleep(.2)
 
-    for _ in range(HEIGHT):
-        sm.refresh()
-        widget.scroll()
-        await sm.sleep(.2)
-
-sm.run_soon(roll_and_scroll())
-sm.run()
-sm.close()
+    sm.run(roll_and_scroll())
