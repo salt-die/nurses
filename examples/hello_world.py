cead6a8bb96ef47c76495816000fa6e734445594
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-31 08:58:33 -0600

    reorganize

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,11 +1,11 @@
 from nurses import ScreenManager
 
 with ScreenManager() as sm:
-    async def marching_border():
-        # Here we demonstrate how to use numpy indexing to simplify animating text marching around a border.
-        widget = sm.new_widget(5, 25, 3, 15, sm.colors.YELLOW_ON_BLACK)
-        widget[0, :13] = "Hello, World!"
+    widget = sm.new_widget(5, 25, 3, 15, sm.colors.YELLOW_ON_BLACK)
+    widget[0, :13] = "Hello, World!"
 
+    # Here we demonstrate how to use numpy indexing to simplify animating text marching around a border.
+    async def marching_border():
         async for _ in sm.delayed(range(120), .1):
             # Move each edge clockwise then place the 3 values that got overwritten.
             tr, bl, br = widget[0, -1], widget[-1, 0], widget[-1, -1]
