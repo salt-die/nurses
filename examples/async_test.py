e1116007a2bae6ba68cd656b64331d619e267f1f
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-31 01:03:49 -0600

    border argument used

--- examples/async_test.py
+++ examples/async_test.py
@@ -5,18 +5,15 @@ with ScreenManager() as sm:
     sm.colors.PURPLE = 103, 15, 215  # Define a new color
 
     for i in range(5):
-        widget = sm.new_widget(i * 5, 0, 4, 15, group="moving")
-        widget.border(color=sm.colors.PURPLE_ON_BLACK)  # Color pair will be initialized automatically if it doesn't exist.
+        widget = sm.new_widget(i * 5, 0, 4, 15, group="moving", border=(sm.colors.PURPLE_ON_BLACK,))
         widget[:] = "Hello, World!\nI'm a widget!"
         widget.colors[0] = sm.colors.RED_ON_BLACK
 
-    resize_widget = sm.new_widget(9, 40, 12, 18, default_color=sm.colors.YELLOW_ON_BLACK)
-    resize_widget.border("curved", sm.colors.RED_ON_BLACK)
+    resize_widget = sm.new_widget(9, 40, 12, 18, sm.colors.YELLOW_ON_BLACK, border=("curved", sm.colors.RED_ON_BLACK))
     resize_widget[:] = "Resize me!"
     resize_widget.colors[:, ::2] = sm.colors.BLACK_ON_YELLOW
 
-    scroll_widget = sm.new_widget(9, 60, 18, 12)
-    scroll_widget.border("double", sm.colors.CYAN_ON_BLACK)
+    scroll_widget = sm.new_widget(9, 60, 18, 12, border=("double", sm.colors.CYAN_ON_BLACK))
     scroll_widget[:] = "Scroll me!"
 
     async def random_walk():
@@ -36,9 +33,9 @@ with ScreenManager() as sm:
             resize_widget.width -= 1
 
     async def scroll():
+        scroll_widget.color = sm.colors.BLUE_ON_BLACK
         async for i in sm.delayed(range(15), 1):
             scroll_widget.scroll()
-            scroll_widget.colors[-1] = sm.colors.BLUE_ON_BLACK
             scroll_widget[-1] = f"New Text{i:02}"
 
     sm.run(random_walk(), roll(), resize(), scroll())
