67b15d58349b9d04e4dad8372072d9b77d04303f
salt-die <53280662+salt-die@users.noreply.github.com>
2021-01-03 15:55:15 -0600

    color test added

--- examples/exploding_logo.py
+++ examples/exploding_logo.py
@@ -7,7 +7,7 @@ Directions:
     arrow-keys to move
     space to poke
 """
-from math import hypot
+from math import hypot, pi, sin
 from pathlib import Path
 
 from nurses import ScreenManager
@@ -21,6 +21,12 @@ MAX_VELOCITY = 4
 FRICTION = .97
 HEIGHT, WIDTH = 27, 56
 PARTICLE_DELAY = .01
+COLORS = 20
+
+def rainbow_rgbs(n=COLORS):
+    offsets = 0, 2 * pi / 3, 4 * pi / 3
+    for i in range(n):
+        yield tuple(int(sin(2 * pi / n * i + offset) * 127 + 128) for offset in offsets)
 
 
 class Cursor(Widget):
@@ -42,12 +48,14 @@ class Cursor(Widget):
 
 
 class Pokable(Widget):
-    def __init__(self, *args, cursor, **kwargs):
+    def __init__(self, *args, cursor, current_color, **kwargs):
         super().__init__(*args, **kwargs)
         self.cursor = cursor
         self.vy = self.vx = 0  # velocity
         self.y = self.sy = self.top
         self.x = self.sx = self.left
+        self.current_color = current_color
+        self.colors[:] = sm.colors.palette["rainbow"][current_color]
         sm.schedule(self.step, delay=PARTICLE_DELAY)
 
     def on_press(self, key):
@@ -88,6 +96,10 @@ class Pokable(Widget):
         self.vy = vy * FRICTION
         self.vx = vx * FRICTION
 
+        self.current_color = (self.current_color + min(mag, MAX_VELOCITY)) % COLORS
+        self.colors[:] = sm.colors.palette["rainbow"][int(self.current_color)]
+        self.refresh()
+
     async def reset(self):
         self.vy = self.vx = 0
         async for i in sm.delayed(range(1, 101), PARTICLE_DELAY):
@@ -100,28 +112,29 @@ class Pokable(Widget):
             if self.top == self.sy and self.left == self.sx:
                 return
 
+
 if __name__ == "__main__":
     with open(Path(__file__).parent / "python_logo.txt") as f:
         logo = f.read()
 
     logo = np.array([list(line + (WIDTH - len(line)) * " ") for line in logo.splitlines()])
+    colors = np.full((HEIGHT, WIDTH), 13)
+    colors[-7:] = colors[-13: -7, -41:] = colors[-14, -17:] = colors[-20: -14, -15:] = 2
 
     with ScreenManager() as sm:
+        for rgb in rainbow_rgbs():
+            sm.colors.pair(rgb, sm.colors._names_to_rgb["BLACK"], palette="rainbow")
+
         cursor = sm.new_widget(0, 0, 3, 3, transparent=True, create_with=Cursor)
         cursor[(0, -1), 1] = "|"
         cursor[1] = "-+-"
 
         # Setup the "particles"
-        colors = np.full((HEIGHT, WIDTH), sm.colors.BLUE_ON_BLACK)
-        colors[-7:] = sm.colors.YELLOW_ON_BLACK
-        colors[-13: -7, -41:] = sm.colors.YELLOW_ON_BLACK
-        colors[-14, -17:] = sm.colors.YELLOW_ON_BLACK
-        colors[-20: -14, -15:] = sm.colors.YELLOW_ON_BLACK
         it = np.nditer((logo, colors), ["multi_index"])
         for char, color in it:
             y, x = it.multi_index
             if char != " ":
-                particle = sm.new_widget(y, x, 1, 1, color=color, cursor=cursor, create_with=Pokable)
+                particle = sm.new_widget(y, x, 1, 1, current_color=color, cursor=cursor, create_with=Pokable)
                 particle[:] = str(char)
 
         sm.top(cursor)
