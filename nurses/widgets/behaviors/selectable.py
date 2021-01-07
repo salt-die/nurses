0cb1582abc3c042e8f2bc5b3d6f9f17ae1cf7c04
salt-die <53280662+salt-die@users.noreply.github.com>
2021-01-06 23:46:36 -0600

    removed print

--- nurses/widgets/behaviors/selectable.py
+++ nurses/widgets/behaviors/selectable.py
@@ -22,12 +22,11 @@ class Selectable:
         return Selectable.__selected is self
 
     def on_press(self, key):
-        print(self, Selectable.__selected)
         if key == self.SELECT_KEY and self.is_selected:
-            while Selectable.__selectables[-1] is not self:
-                Selectable.__selectables.rotate()
-            Selectable.__selectables.rotate()
-            Selectable.__selected = Selectable.__selectables[-1]
+            while Selectable.__selectables[0] is not self:
+                Selectable.__selectables.rotate(-1)
+            Selectable.__selectables.rotate(-1)
+            Selectable.__selected = Selectable.__selectables[0]
             return True
 
         return super().on_press(key)
