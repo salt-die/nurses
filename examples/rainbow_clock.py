151b22ba109f4cb6eb09becd25734b8607b6a4c3
salt-die <53280662+salt-die@users.noreply.github.com>
2021-01-09 12:00:47 -0600

    "self-document"

--- examples/rainbow_clock.py
+++ examples/rainbow_clock.py
@@ -8,14 +8,16 @@ class MovableClock(DigitalClock, Movable):
     ...
 
 with ScreenManager() as sm:
-    sm.root.add_widget(MovableClock(0, 0))
+    widget = sm.root.new_widget(0, 0, create_with=MovableClock)
 
     colors.rainbow_gradient(20)
+    rainbow = colors.palette["rainbow"]
 
     async def rainbow_time():
+
         i = 0
         while True:
-            sm.root.children[0].update_color(colors.palette["rainbow"][i])
+            widget.update_color(rainbow[i])
             i = (i + 1) % 20
             sm.root.refresh()
             await sm.sleep(.1)
