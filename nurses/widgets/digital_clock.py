4627352fa89ccdae04b148bccbbab4f6d7aa5d10
salt-die <53280662+salt-die@users.noreply.github.com>
2021-01-09 15:36:20 -0600

    option for twelve hour clock

--- nurses/widgets/digital_clock.py
+++ nurses/widgets/digital_clock.py
@@ -74,18 +74,23 @@ DIGITS = ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
 def format(n):
     return DIGITS[n // 10], DIGITS[n % 10]
 
-def digital_time():
-    hours, minutes, seconds = map(format, time.localtime()[3:6])
+def digital_time(twelve_hour=False):
+    hours = time.localtime()[3]
+    if twelve_hour:
+        hours %= 12
+    hours, minutes, seconds = map(format, (hours, *time.localtime()[4:6]))
+
     return *hours, COLON, *minutes, COLON, *seconds
 
 
 class DigitalClock(Widget):
     """A digital clock widget.  Dimensions of this widget are (3, 24).
     """
-    def __init__(self, top, left, *args, **kwargs):
+    def __init__(self, top, left, *args, twelve_hour=False, **kwargs):
         super().__init__(top, left, 3, 24)
+        self.twelve_hour = twelve_hour
 
     def refresh(self):
-        for x, digit in enumerate(digital_time()):
+        for x, digit in enumerate(digital_time(self.twelve_hour)):
             for y, line in enumerate(digit):
                 self.window.addstr(y, x * 3, line)
