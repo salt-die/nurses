63ebe4828a8a779cc51f72a60e90f6180763c36c
salt-die <53280662+salt-die@users.noreply.github.com>
2021-01-22 19:43:40 -0600

    optional args

--- nurses/widgets/grid.py
+++ nurses/widgets/grid.py
@@ -4,15 +4,20 @@ from .layout import Layout
 class Grid(Layout):
     def __init__(self, *args, rows, cols,**kwargs):
         super().__init__(*args, **kwargs)
+        self._rows = rows
+        self._cols = cols
         self.children = [None for _ in range(rows * cols)]
 
-    def add_widget(self, widget):
-        for i, child in enumerate(self.children):
-            if child is None:
-                self.children[i] = widget
-                break
+    def add_widget(self, widget, row=None, col=None):
+        if row is None and col is None:
+            for i, child in enumerate(self.children):
+                if child is None:
+                    self.children[i] = widget
+                    break
+            else:
+                raise ValueError("too many children")
         else:
-            raise ValueError("too many children")
+            self.children[self._cols * row + col] = widget
 
         widget.parent = self
         widget.update_geometry()
