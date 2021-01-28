7ddeb2fd181a4ddc2c17959572ed0ff68c53554a
salt-die <53280662+salt-die@users.noreply.github.com>
2021-01-28 12:28:55 -0600

    size hint

--- examples/pad_test.py
+++ examples/pad_test.py
@@ -8,7 +8,7 @@ class ScrollingPad(ArrayPad, Scrollable):
 with ScreenManager() as sm:
     blue_to_white = colors.gradient(200, (0, 0, 255), (255, 255, 255), "blue_to_white")
 
-    scroll_pad = sm.root.new_widget(create_with=ScrollingPad, rows=200, cols=200)
+    scroll_pad = sm.root.new_widget(create_with=ScrollingPad, rows=200, cols=200, size_hint=(1.0, 1.0))
 
     for i in range(200):
         scroll_pad[i, :] = f'{f"{i:03}":>10}' * 20
