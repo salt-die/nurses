04c5bafd720c009f74701eab5e7aef9f40b16e1f
salt-die <53280662+salt-die@users.noreply.github.com>
2021-01-30 07:50:28 -0600

    loop over colors

--- examples/pad_test.py
+++ examples/pad_test.py
@@ -6,16 +6,14 @@ class ScrollingPad(ArrayPad, Scrollable):
     ...
 
 with ScreenManager() as sm:
-    blue_to_white = colors.gradient(200, (0, 0, 255), (255, 255, 255), "blue_to_white")
-
     scroll_pad = sm.root.new_widget(
         create_with=ScrollingPad, rows=200, cols=200, size_hint=(1.0, 1.0),
         right_scrollbar=True, bottom_scrollbar=True, bar_color=colors.BLUE_ON_WHITE
     )
 
-    for i in range(200):
+    for i, color in enumerate(colors.gradient(200, (0, 0, 255), (255, 255, 255), "blue_to_white")):
         scroll_pad[i, :] = f'{f"{i:03}":>10}' * 20
-        scroll_pad.pad_colors[i] = blue_to_white[i]
+        scroll_pad.pad_colors[i] = color
 
     sm.schedule(sm.root.refresh)
     sm.run()
