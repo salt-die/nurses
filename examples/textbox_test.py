dc3767b20802db5def22b862aec3152b6f6cbaf5
salt-die <53280662+salt-die@users.noreply.github.com>
2021-02-03 12:12:38 -0600

    redudant refresh

--- examples/textbox_test.py
+++ examples/textbox_test.py
@@ -3,10 +3,9 @@ from nurses.widgets import Textbox
 
 
 with ScreenManager() as sm:
-    tb = sm.root.new_widget(0, 0, 20, border="light", create_with=Textbox)
+    tb = sm.root.new_widget(0, 0, 20, border="curved", create_with=Textbox)
 
     async def print_result():
         print(await tb.gather())
 
-    sm.root.refresh()
     sm.run(print_result())
