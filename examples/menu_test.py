db91343466f0f8433d39881ffa79f54aed8980ca
salt-die <53280662+salt-die@users.noreply.github.com>
2021-02-11 16:55:32 -0600

    enough implementation that menu works; menu_test added

---
+++ examples/menu_test.py
@@ -0,0 +1,20 @@
+from nurses import ScreenManager, colors
+from nurses.widgets import Menu
+
+
+class MyMenu(Menu):
+    open_close_key = ord('`')
+
+
+with ScreenManager() as sm:
+    sm.root.add_widget(
+        MyMenu(0, 0, "Menu",
+            (
+                ("1st Entry", lambda: print("1st Entry Selected")),
+                ("2nd Entry", lambda: print("2nd Entry Selected")),
+                ("3rd Entry", lambda: print("3rd Entry Selected")),
+            ),
+            border_style="curved"))
+
+    sm.schedule(sm.root.refresh)
+    sm.run()
--- nurses/widgets/menu.py
+++ nurses/widgets/menu.py
@@ -16,19 +16,35 @@ class Menu(Widget):
     move_up = UP
     move_down = DOWN
     select_key = ENTER
-    open_close_key = None  # Alt + first letter of menu name?
-
-    wrap = False
+    open_close_key = None
 
     selected_color = None
 
-    def __init__(self, top, left, name, *items, **kwargs):
-        raise NotImplementedError
+    def __init__(self, top, left, name, items, **kwargs):
         self.name = name
-        self._items = dict(items)
 
-        offset = bool(kwargs.get("border"))
-        super().__init__(top, left, len(self._items) + 2 * offset, max(map(len, self._items) + 2 * offset, **kwargs))
+        self._items = []
+        self._callbacks = []
+        for item, callback in items:
+            self._items.append(item)
+            self._callbacks.append(callback)
+
+        offset = bool(kwargs.get("border_style"))
+        super().__init__(top, left, len(self._items) + 2 * offset, max(map(len, self._items)) + 2 * offset, **kwargs)
+
+    @property
+    def is_closed(self):
+        return self.window is self._closed_window
+
+    def __len__(self):
+        return len(self._items)
+
+    def open_menu(self):
+        self.window = self._open_window
+        self._selected_entry = 0
+
+    def close_menu(self):
+        self.window = self._closed_window
 
     def update_geometry(self):
         if self.root is None:
@@ -38,12 +54,51 @@ class Menu(Widget):
             from .. import colors
             self.selected_color = colors.BLACK_ON_WHITE
 
-        h, w = self.parent.height, self.parent.width
-
         if self.window is None:
-            self.window = self._closed_window = curses.newwin(1, self.width + 1)
+            self._closed_window = curses.newwin(1, self.width + 1)
+            self._closed_window.attrset(self.color)
+            self._closed_window.addstr(0, 0, self.name)
+
             self._open_window = curses.newwin(self.height, self.width + 1)
-            self.update_color(self.color)
+            self._open_window.attrset(self.color)
+
+            self.window = self._open_window  # Temporarily set `window` to open so we can set a border if needed
+            if self.has_border:
+                self.border(self.border_style, self.border_color)
+                offset = 1
+            else:
+                offset = 0
+
+            for i, item in enumerate(self._items, start=offset):
+                self._open_window.addstr(i, offset, item)
+
+            self.window = self._closed_window
+
+    def refresh(self):
+        if self.is_closed:
+            return
+
+        offset = int(self.has_border)
+        for i, item in enumerate(self._items):
+            self._open_window.chgat(i + offset, offset, len(item), self.color if i != self._selected_entry else self.selected_color)
 
     def on_press(self, key):
-        ...
+        if key == self.open_close_key:
+            self.open_menu() if self.is_closed else self.close_menu()
+            return True
+
+        if self.is_closed:
+            return
+
+        if key == self.move_up:
+            self._selected_entry = (self._selected_entry - 1) % len(self)
+        elif key == self.move_down:
+            self._selected_entry = (self._selected_entry + 1) % len(self)
+        elif key == self.select_key:
+            self._callbacks[self._selected_entry]()
+            self.close_menu()
+        else:
+            return
+
+        self.root.refresh()
+        return True
