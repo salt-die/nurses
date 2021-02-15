8e3ca63dde519311874e116f8d9ebf209021f389
salt-die <53280662+salt-die@users.noreply.github.com>
2021-02-15 12:05:36 -0600

    account for possible border

--- nurses/widgets/behaviors/scrollable.py
+++ nurses/widgets/behaviors/scrollable.py
@@ -10,14 +10,16 @@ class Scrollable:
     scroll_right = RIGHT
 
     def on_press(self, key):
+        border_width = self.has_border * 2
+
         if key == self.scroll_up:
             self.min_row = max(0, self.min_row - 1)
         elif key == self.scroll_down:
-            self.min_row = min(self.rows - (self.height - self.top_scrollbar - self.bottom_scrollbar), self.min_row + 1)
+            self.min_row = min(self.rows - (self.height - self.top_scrollbar - self.bottom_scrollbar - border_width), self.min_row + 1)
         elif key == self.scroll_left:
             self.min_col = max(0, self.min_col - 1)
         elif key == self.scroll_right:
-            self.min_col = min(self.cols - (self.width - self.left_scrollbar - self.right_scrollbar), self.min_col + 1)
+            self.min_col = min(self.cols - (self.width - self.left_scrollbar - self.right_scrollbar - border_width), self.min_col + 1)
         else:
             return super().on_press(key)
 
