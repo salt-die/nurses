2cbb803d5c02eed43e03e14a1c2d078f86159460
salt-die <53280662+salt-die@users.noreply.github.com>
2021-02-24 07:08:27 -0600

    extra aliases defined in on_press to make conditional block easier to read

--- nurses/widgets/behaviors/scrollable.py
+++ nurses/widgets/behaviors/scrollable.py
@@ -17,19 +17,23 @@ class Scrollable:
 
     def on_press(self, key):
         border_width = self.has_border * 2
+        height_offset = self.top_scrollbar + self.bottom_scrollbar
+        page = self.buffer.shape[0] - 1 - height_offset
+        min_row, min_col = self.min_row, self.min_col
+        max_min_row = self.rows - (self.height - height_offset - border_width)
 
         if key == self.scroll_up or key == self.scroll_up_alt:
-            self.min_row = max(0, self.min_row - 1)
+            self.min_row = max(0, min_row - 1)
         elif key == self.scroll_down or key == self.scroll_down_alt:
-            self.min_row = min(self.rows - (self.height - self.top_scrollbar - self.bottom_scrollbar - border_width), self.min_row + 1)
+            self.min_row = min(max_min_row, min_row + 1)
         elif key == self.scroll_left or key == self.scroll_left_alt:
-            self.min_col = max(0, self.min_col - 1)
+            self.min_col = max(0, min_col - 1)
         elif key == self.scroll_right or key == self.scroll_right_alt:
-            self.min_col = min(self.cols - (self.width - self.left_scrollbar - self.right_scrollbar - border_width), self.min_col + 1)
+            self.min_col = min(self.cols - (self.width - self.left_scrollbar - self.right_scrollbar - border_width), min_col + 1)
         elif key == PGUP:
-            self.min_row = max(0, self.min_row - self.buffer.shape[0] - 1)
+            self.min_row = max(0, min_row - page)
         elif key == PGDN:
-            self.min_row = min(self.rows - (self.height - self.top_scrollbar - self.bottom_scrollbar - border_width), self.min_row + self.buffer.shape[0] - 1)
+            self.min_row = min(max_min_row, min_row + page)
         else:
             return super().on_press(key)
 
