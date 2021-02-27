1e26080d1dbddbc056330847379fb7b7ae07c623
salt-die <53280662+salt-die@users.noreply.github.com>
2021-02-27 09:51:29 -0600

    added a menu to one window

--- examples/behaviors_test.py
+++ examples/behaviors_test.py
@@ -2,7 +2,7 @@ from itertools import cycle
 from random import random
 
 from nurses import colors, ScreenManager
-from nurses.widgets import ArrayWin, Chart, DigitalClock, TextPad
+from nurses.widgets import ArrayWin, DigitalClock, TextPad
 from nurses.widgets.behaviors import Selectable, Movable, Bouncing
 
 
@@ -43,12 +43,40 @@ with ScreenManager() as sm:
     blue_to_purple = colors.gradient(20, (0, 255, 255), (103, 15, 215), "blue_to_purple")
 
     sm.root.add_widget(Notepad(0, 0, size_hint=(.5, .5)))
-    sm.root.add_widget(Notepad(pos_hint=(0, .5), size_hint=(.5, .5)))
-
+    ur = sm.root.new_widget(pos_hint=(.0, .5), size_hint=(.5, .5), create_with=Window)
     lr = sm.root.new_widget(pos_hint=(.5, .0), size_hint=(.5, .5), create_with=Window)
-    chart = lr.new_widget(maxlen=200, gradient=blue_to_purple, y_label=5, size_hint=(1., 1.), create_with=Chart)
-
     ll = sm.root.new_widget(pos_hint=(.5, .5), size_hint=(.5, .5), create_with=Window)
+
+    # Setup Menubar in upper-right window
+    ur._out = 1
+    def add_text(text):
+        if ur._out == len(ur.buffer[:, 0]):
+            ur.scroll()
+        else:
+            ur._out += 1
+
+        ur[ur._out - 1, :len(text)] = text
+
+    menu = (
+        ("1st Entry", lambda: add_text("1st Entry Selected")),
+        ("2nd Entry", lambda: add_text("2nd Entry Selected")),
+        ("3rd Entry", lambda: add_text("3rd Entry Selected")),
+    )
+
+    ur.new_widget(
+        ("Menu1", menu),
+        ("Menu2", menu),
+        ("Menu3", menu),
+        color=colors.BLACK_ON_WHITE,
+        selected_color=colors.WHITE_ON_BLACK,
+        border_style="light",
+        create_with="Menubar",
+    )
+
+    # Chart to lower-right window
+    chart = lr.new_widget(maxlen=200, gradient=blue_to_purple, y_label=5, size_hint=(1., 1.), create_with="Chart")
+
+    # Moving clock to lower-left window
     mc = ll.new_widget(1, 1, delay=.1, create_with=MovingClock)
     mc.schedule_bounce()
 
