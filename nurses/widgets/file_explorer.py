e65b3edab65048450e67d565725cfe9e2b1a0b56
salt-die <53280662+salt-die@users.noreply.github.com>
2021-03-01 11:40:15 -0600

    directory sorting utility function added

--- nurses/widgets/file_explorer.py
+++ nurses/widgets/file_explorer.py
@@ -14,14 +14,36 @@ class FileExplorer(Widget):
     page_down = PGDN
 
     def __init__(self, *args, **kwargs):
+        self.close_explorer()  # Should be before super call so that self.root is None and root.remove_widget doesn't error
+        super().__init__(*args, **kwargs)
         raise NotImplementedError
-        ...
 
     def open_explorer(self):
-        ...
+        if self.root is not None:
+            self.root.add_widget(self)
 
     def close_explorer(self):
-        ...
+        self._current_path = Path("")
+
+        if self.root is not None:
+            self.root.remove_widget(self)
 
     def refresh(self):
+        ...
+
+    def _get_directory(self):
+        directories = []
+        files = []
+        for child in self._current_path.iterdir():
+            if child.is_dir():
+                directories.append(child)
+            else:
+                files.append(child)
+
+        directories.sort(key=lambda path: path.name)
+        files.sort(lambda path: path.name)
+
+        return directories + files
+
+    def on_press(self, key):
         ...
