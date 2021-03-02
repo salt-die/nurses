efd3570e60a2b6afe691e1b4d995dcba45f5c87d
salt-die <53280662+salt-die@users.noreply.github.com>
2021-03-02 06:40:45 -0600

    inherit from ArrayPad instead of Widget

--- nurses/widgets/file_explorer.py
+++ nurses/widgets/file_explorer.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
-from . import Widget
+from . import ArrayPad
+from .behaviors import Scrollable
 from .. import UP, UP_2, DOWN, DOWN_2, ENTER, PGUP, PGDN
 
 
-class FileExplorer(Widget):
+class FileExplorer(ArrayPad, Scrollable):
     move_up = UP
     move_up_alt = UP_2
     move_down = DOWN
@@ -14,13 +15,16 @@ class FileExplorer(Widget):
     page_down = PGDN
 
     def __init__(self, *args, **kwargs):
+        raise NotImplementedError
+
         self.close_explorer()  # Should be before super call so that self.root is None and root.remove_widget doesn't error
         super().__init__(*args, **kwargs)
-        raise NotImplementedError
 
     def open_explorer(self):
         if self.root is not None:
             self.root.add_widget(self)
+
+        self.is_open = True
 
     def close_explorer(self):
         self._current_path = Path("")
@@ -28,8 +32,14 @@ class FileExplorer(Widget):
         if self.root is not None:
             self.root.remove_widget(self)
 
+        self.is_open = False
+
     def refresh(self):
-        ...
+        if not self.is_open:
+            return
+
+        border_offset = int(self.has_border)
+        current_directory = self._get_directory()
 
     def _get_directory(self):
         directories = []
@@ -46,4 +56,7 @@ class FileExplorer(Widget):
         return directories + files
 
     def on_press(self, key):
-        ...
+        if key == self.select_key:
+            ...
+
+        return super().on_press(key)
