b20074f18dda93ccf1e1fe7f415aa3555322afb0
salt-die <53280662+salt-die@users.noreply.github.com>
2021-03-09 21:04:39 -0600

    Changed colors for menubars; added scrollbar

--- examples/notepad.py
+++ examples/notepad.py
@@ -4,12 +4,15 @@ NOT_IMPLEMENTED = lambda: None
 
 
 with ScreenManager() as sm:
+    blue = colors.WHITE_ON_BLUE
+    white = colors.BLUE_ON_WHITE
+
     fe = sm.root.new_widget(size_hint=(.5, .5), pos_hint=(.25, .25), border_style="light", create_with="FileExplorer")
     fe.close_explorer()
     de = sm.root.new_widget(size_hint=(.5, .5), pos_hint=(.25, .25), border_style="light", create_with="DirExplorer")
     de.close_explorer()
 
-    tp = sm.root.new_widget(1, 0, size_hint=(None, 1.0), create_with="TextPad")
+    tp = sm.root.new_widget(1, 0, size_hint=(None, 1.0), right_scrollbar=True, bar_color=blue, create_with="TextPad")
     tp.getter("height", lambda: sm.root.height - 1)
     tp._resize()
 
@@ -37,8 +40,8 @@ with ScreenManager() as sm:
     menubar = sm.root.new_widget(
         ("File", file),
         ("Edit", edit),
-        color=colors.BLACK_ON_WHITE,
-        selected_color=colors.WHITE_ON_BLACK,
+        color=blue,
+        selected_color=white,
         border_style="curved",
         create_with="Menubar"
     )
